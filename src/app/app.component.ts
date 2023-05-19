fdd3485dd36b168b6d969a95f8315f0f14644c9b
Lisa Van Huijkelom <lisa.vanhuijkelom@infinicept.com>
2023-05-18 20:09:32 -0600

    init commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,60 @@
 import { Component } from '@angular/core';
+import { MenuItem } from './menu-Item';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'ChirpusPizzaLab';
-}
+
+  menu: MenuItem[] = [
+    {
+      Name: "Chciken Alfredo",
+      Category: "Pasta",
+      Price: 11.99
+    },
+    {
+      Name: "Baked Ziti",
+      Category: "Pasta",
+      Price: 10.99
+    },
+    {
+      Name: "Chicken Pesto Penne",
+      Category: "Pasta",
+      Price: 12.99
+    },
+    {
+      Name: "Cheesy Cheese",
+      Category: "Pizza",
+      Price: 10.00
+    },
+    {
+      Name: "Peperoniskies",
+      Category: "Pizza",
+      Price: 12.99
+    },
+    {
+      Name: "Veggie Me",
+      Category: "Pizza",
+      Price: 9.99
+    },
+    {
+      Name: "Pizza Cookie",
+      Category: "Dessert",
+      Price: 8.99
+    },
+    {
+      Name: "Strawberry Maddness Cake",
+      Category: "Dessert",
+      Price: 7.99
+    },
+    {
+      Name: "Ice Cream Delight",
+      Category: "Dessert",
+      Price: 6.99
+    },
+  ]
+  
+  ;}
